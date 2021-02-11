9eba6c03c0a82c7efa1b96c2a6ee00bed1faae38
ClaireCaudwell <clairecaudwell@gmail.com>
2021-02-11 09:45:18 +0100

    Changed name of stylesheet import

--- frontend/src/pages/NotesPage.js
+++ frontend/src/pages/NotesPage.js
@@ -6,17 +6,18 @@ import { AddNote } from "../components/Notes-components/AddNote";
 import { NoteList } from "../components/Notes-components/NoteList";
 import { getNotes, note } from "../reducer/note";
 
-import "./NotesPage.css";
+import  "./NotesPage.css";
 import "./MediaQueries.css";
 
 export const NotesPage = () => {
     const dispatch = useDispatch();
+
     const userId = useSelector((store) => store.user.login.userId);
 
     useEffect(() => {
         dispatch(getNotes(userId));
         dispatch(note.actions.setStatusMessage({ statusMessage: null }));
-    }, [dispatch, userId]);
+    }, [dispatch, userId]); 
 
     return (
         <>
