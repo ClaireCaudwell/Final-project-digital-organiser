2477edde9751aaae387669632765674d4bb28e9b
ClaireCaudwell <clairecaudwell@gmail.com>
2021-02-19 10:07:44 +0100

    Updating footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-import { MainContainer } from "../styled-components/SignupLogin";
+// import { MainContainer } from "../styled-components/SignupLogin";
 import { BasicP } from "../styled-components/GlobalStyle";
 
 
 export const Footer = () => {
     return(
-        <MainContainer>
+        <FooterContainer>
             <BasicP>Digital Organiser created by Claire Caudwell for the Technigo Bootcamp final project</BasicP>
-        </MainContainer>
+        </FooterContainer>
     );
 };
+
+const FooterContainer = styled.div`
+    padding: 20px;
+`;
+
