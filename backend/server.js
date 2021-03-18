98c41a3af15a21257d156b43bb99b754a5904ee1
ClaireCaudwell <clairecaudwell@gmail.com>
2021-03-18 21:28:00 +0100

    Putting schema in seperate file didn't seem to work so I decided to restore it to how it was before.

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,59 @@ import cors from "cors";
 import mongoose from "mongoose";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
-import userSchema from "./schema";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/digitalOrganiser";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const userSchema = new mongoose.Schema( {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 30,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+    unique: true, 
+  },
+  // Array of tasks
+  // subdocument - saved whenever their top-level parent document is saved
+  scheduleTask: [{
+    task: {
+      type: String,
+      minlength: 3,
+      maxlength: 30,
+      required: true,
+    },
+    startdatetime: {
+      type: Date,
+    },
+    delete: {
+      type: Boolean,
+      default: false,
+    }, 
+  }],
+  // Array for users notes
+  notes: [{
+    noteText: {
+      type: String,
+      maxlength: 170,
+    },
+    colour: {
+      type: Number,
+      default: 0, 
+    }
+  }]
+});
 
 const User = mongoose.model("User", userSchema);
 
